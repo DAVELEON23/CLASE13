f944d72b65182d6f566ddfade6575f9bd017971f
DAVELEON23 <davidaarondeleon@gmail.com>
2023-09-17 13:49:36 -0300

    Se crearon 2 archivos js y 1 json

---
+++ array.js
@@ -0,0 +1,12 @@
+let nombres = ["Daniel","Oscar", "Mateo","Pedro"];
+let edades = [36,4,25,53];
+let valoresDeVerdad = [true,false,false,false,true];
+let variados = ["Pizza",200,false,edades];
+
+
+//consultar o llamar un dato dentro de un array. 
+
+// console.log(valoresDeVerdad);
+
+
+//consultar un array dentro de otro array. 
---
+++ calculadora.js
@@ -0,0 +1,8 @@
+let suma = (a,b) => a+b;
+let resta = (a,b) => a-b;
+let multiplicacion = (a,b) => a*b;
+let division = (a,b) => a/b;
+
+function calculadora (a, b, callback) {
+    return callback (a,b,callback);
+}
